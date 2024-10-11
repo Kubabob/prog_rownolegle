588c7290cb214507df7d05df51d055e24cbfe5d9
Kubabob <j.m.m.bozek@gmail.com>
2024-10-11 15:21:56 +0200

    update

--- pierwsze_programy/arrays_passing.py
+++ pierwsze_programy/arrays_passing.py
@@ -6,27 +6,27 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
 # passing MPI datatypes explicitly
-if rank == 0:
+if rank % 2 == 0:
     start = MPI.Wtime()
     data = numpy.arange(1_000_000_000, dtype='i')
-    comm.send(start, dest=1, tag=78)
-    comm.Send([data, MPI.INT], dest=1, tag=77)
-elif rank == 1:
+    comm.send(start, dest=1, tag=100+rank)
+    comm.Send([data, MPI.INT], dest=1, tag=rank)
+elif rank % 2 == 1:
     data = numpy.empty(1_000_000_000, dtype='i')
-    comm.Recv([data, MPI.INT], source=0, tag=77)
-    start = comm.recv(source=0, tag=78)
+    comm.Recv([data, MPI.INT], source=0, tag=rank)
+    start = comm.recv(source=0, tag=100+rank)
     end1 = MPI.Wtime() - start
     print(f"Explicit type took: {end1}s")
 
 # automatic MPI datatype discovery
-if rank == 0:
+if rank % 2 == 0:
     start = MPI.Wtime()
     data = numpy.arange(1_000_000_000, dtype=numpy.float64)
-    comm.send(start, dest=1, tag=78)
-    comm.Send(data, dest=1, tag=13)
-elif rank == 1:
+    comm.send(start, dest=1, tag=100+rank)
+    comm.Send(data, dest=1, tag=rank)
+elif rank % 2 == 1:
     data = numpy.empty(1_000_000_000, dtype=numpy.float64)
-    comm.Recv(data, source=0, tag=13)
-    start = comm.recv(source=0, tag=78)
+    comm.Recv(data, source=0, tag=rank)
+    start = comm.recv(source=0, tag=100+rank)
     end2 = MPI.Wtime() - start
     print(f"Discovered type took: {end2}s")
